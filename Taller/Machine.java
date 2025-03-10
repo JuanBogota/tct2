c867cb3bf794782288b267e9042588f2bc04d293
Juan Bogotá <juan.bogota-f@mail.escuelaing.edu.co>
2025-03-10 09:47:53 -0500

    Add files via upload

---
+++ Taller/Machine.java
@@ -0,0 +1,17 @@
+public class Machine {
+
+    private Position location;
+
+    public Machine(Position location) {
+        this.location = location;
+    }
+
+    public Position getLocation() {
+        return location;
+    }
+
+    public void setLocation(Position location) {
+        this.location = location;
+    }
+
+}
